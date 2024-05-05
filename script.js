17d61af9ec2d36718f38bfd7eb35aae5d715f87c
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-05 22:03:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+bands.sort((a,b)=>{
+	let aArr = a.split(" ")
+	let bArr = b.split(" ")
+	let i = 0
+	let j = 0
+	if(aArr[i].toLowerCase()=="a"||aArr[i].toLowerCase()=="an"||aArr[i].toLowerCase()=="the"){
+		i++
+	}
+	if(bArr[j].toLowerCase()=="a"||bArr[j].toLowerCase()=="an"||bArr[j].toLowerCase()=="the"){
+		j++
+	}
+	if(aArr[i]<bArr[j]){
+		return -1
+	}
+	else if(aArr[i]>bArr[j]){
+		return 1
+	}
+	return 0
+})
+
+let u = document.getElementById("band")
+for (let i of bands) {
+	let l = document.createElement("li")
+	l.innerHTML = `<li>${i}</li>`
+	u.append(l)
+}
